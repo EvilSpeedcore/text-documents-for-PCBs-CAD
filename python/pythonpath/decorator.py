d0a74e237b887a91335925e7413f5e805cbe0d11
EvilSpeedcore <32939671+EvilSpeedcore@users.noreply.github.com>
2017-11-06 16:31:20 +0300

    Update decorator.py
    calculate_cell_range function added
    set_style function now not as ugly as it was

--- python/pythonpath/decorator.py
+++ python/pythonpath/decorator.py
@@ -23,24 +23,28 @@ class DataDecorator():
     def set_optimal_row_height(self):
         """Set optimal height property of table row."""
         for index, item in enumerate(self.data.designator):
-            cell_name = self.colons["designator"] + str(index + self.first_row_index)
+            cell_name = ''.join([self.colons["designator"], str(index + self.first_row_index)])
             cell = self.document.table.getCellByName(cell_name)
             cell_text = cell.getString()
             if cell_text:
                 row = self.document.rows.getByIndex(index + self.first_row_index - 1)
                 row.OptimalHeight = True
 
+    def calculate_cell_range(self):
+        """Calculate cell range in document table for formatting."""
+        range_list = []
+        for value in self.colons.values():
+            start = ''.join([value, str(self.first_row_index)])
+            finish = ''.join([value, str(self.last_row_index)])
+            complete_range = ':'.join([start, finish])
+            range_list.append(complete_range)
+        return range_list
+
     def set_style(self):
         """Set style of table which contains converted data from BOM-file."""
-        _range1 = self.document.table.getCellRangeByName(self.colons["list_of_components"] + str(self.first_row_index)
-                                                         + ":" + self.colons["list_of_components"]
-                                                         + str(self.last_row_index))
-        _range2 = self.document.table.getCellRangeByName(self.colons["quantity"] + str(self.first_row_index)
-                                                         + ":" + self.colons["quantity"]
-                                                         + str(self.last_row_index))
-        _range3 = self.document.table.getCellRangeByName(self.colons["designator"] + str(self.first_row_index)
-                                                         + ":" + self.colons["designator"]
-                                                         + str(self.last_row_index))
+        list_of_components_range, quantity_range, designator_range = self.calculate_cell_range()
+        _range1 = self.document.table.getCellRangeByName(list_of_components_range)
+        _range2 = self.document.table.getCellRangeByName(quantity_range)
+        _range3 = self.document.table.getCellRangeByName(designator_range)
         _range1.CharPosture, _range2.CharPosture, _range3.CharPosture = (FS_ITALIC, FS_ITALIC, FS_ITALIC)
-        _range1.paraAdjust = LEFT
-        _range2.paraAdjust, _range3.paraAdjust = (CENTER, CENTER)
+        _range1.ParaAdjust, _range2.ParaAdjust, _range3.ParaAdjust = (LEFT, CENTER, CENTER)
